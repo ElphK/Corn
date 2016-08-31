5af16ef2ced8202cd45c11dcc8a0edaa0e08dcca
ElphK <ef.technology21@gmail.com>
2016-08-31 17:08:39 +0200

    Добавление файлов

---
+++ CornModel/Abstract/BindableBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Windows.Threading;
+
+
+namespace CornModel.Abstract
+{
+  public  abstract class BindableBase : INotifyPropertyChanged
+    {
+
+        protected virtual void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            PropertyChangedEventHandler changedEventHandler = PropertyChanged;
+            if (changedEventHandler == null)
+                return;
+            string propertyName = GetPropertyName(propertyExpression);
+            changedEventHandler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected static string GetPropertyName<T>(Expression<Func<T>> propertyExpression)
+        {
+        //    if (propertyExpression == null)
+        //        throw new ArgumentNullException("propertyExpression");
+
+            MemberExpression memberExpression = propertyExpression.Body as MemberExpression;
+        //    if (memberExpression == null)
+        //        throw new ArgumentException(@"Не правильный аргумент", "propertyExpression");
+
+            PropertyInfo propertyInfo = memberExpression.Member as PropertyInfo;
+            //if (propertyInfo == null)
+            //    throw new ArgumentException(@"Не правильный аргумент", "propertyExpression");
+            return propertyInfo.Name;
+        }
+
+        protected bool Set<T>(Expression<Func<T>> propertyExpression, ref T field, T newValue)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+                return false;
+            field = newValue;
+            RaisePropertyChanged(propertyExpression);
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    //    [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+        /// <summary>
+        /// Выполнить в потоке UI
+        /// </summary>
+        /// <param name="action">Метод, который должен выполниться</param>
+        protected virtual void UiInvoke(Action action)
+        {
+            if (Dispatcher.CurrentDispatcher.CheckAccess()) action();
+            else Dispatcher.CurrentDispatcher.BeginInvoke(action);
+        }
+    }
+}
---
+++ CornModel/Converters/TestConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using CornModel.Abstract;
+using System.Windows;
+
+namespace CornModel.Converters
+{
+    public class TestConverter : ConverterBase<TestConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+          var rez = 60;
+          if ((bool)value) rez = 200; 
+          return rez;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+
+    public class BoolToVisibleConverter : ConverterBase<BoolToVisibleConverter>
+    {
+      
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility visibility = Visibility.Collapsed;
+            if (parameter != null && parameter.Equals("Hidden")) visibility = Visibility.Hidden;
+            return (bool)value ? Visibility.Visible : visibility;
+        }
+    }
+}
